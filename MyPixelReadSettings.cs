bd9d82d5bcf12b153c8715ef48319b40a8c3daf3
Minshu Gurjar <minshugurjar@gmail.com>
2023-02-18 00:59:30 +0530

    yes

---
+++ MyPixelReadSettings.cs
@@ -0,0 +1,19 @@
+﻿using ImageMagick;
+
+#if Q8
+using QuantumType = System.Byte;
+#elif Q16
+using QuantumType = System.UInt16;
+#elif Q16HDRI
+using QuantumType = System.Single;
+#else
+#endif
+
+namespace HeightMapExtractor;
+
+public class MyPixelReadSettings: IPixelReadSettings<ushort>
+{
+    public string? Mapping { get; set; }
+    public StorageType StorageType { get; set; }
+    public IMagickReadSettings<ushort> ReadSettings { get; set; }
+}
